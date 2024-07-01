cda42d7ead6bbb934d2d3c84e5890225b4267e2d
Rakesh <rakesh.007ac@gmail.com>
2024-07-01 20:10:16 +0530

    basic layout

--- script.js
+++ script.js
@@ -14,11 +14,17 @@ function confirmPwd(){
         confPass.style.borderColor = "#CF001F";
 
     }
-    else{
+    if(pass.length==0){
+        password.style.borderColor = "#E5E6EB";
+        confPass.style.borderColor = "#E5E6EB";
+        error.style.display = "none";
+    }
+    if(pass.length>=8&&pass==confirm){
         password.style.borderColor = "green";
         confPass.style.borderColor = "green";
         error.style.display = "none";
     }
+    
 }
 
 document.getElementById("conf-pwd").addEventListener('input', confirmPwd);
